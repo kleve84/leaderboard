9696d955cd2de205735db0a69ab921b9c35785f4
Kyle <kleve84@hawaii.edu>
2017-10-10 14:30:39 -1000

    Task 5:Events, Sessions

---
+++ app/leaderboard.js
@@ -0,0 +1,29 @@
+PlayersList = new Mongo.Collection('players');
+
+if (Meteor.isClient) {
+  Template.leaderboard.helpers({
+    'player': function () {
+      return PlayersList.find();
+    },
+    'count': function () {
+      return PlayersList.find().count();
+    },
+    'selectedClass': function () {
+      var playerId = this._id;
+      var selectedPlayer = Session.get('selectedPlayer');
+      if (playerId == selectedPlayer) {
+        return "selected"
+      }
+    }
+  });
+  Template.leaderboard.events({
+    'click .player': function () {
+      var playerId = this._id;
+      Session.set('selectedPlayer', playerId);
+    }
+  });
+}
+
+if (Meteor.isServer) {
+
+}
